ede4d5d3625e84ebff4f0844baa61afa117c9293
Marco Rojo <mrmarcondes@gmail.com>
2016-08-23 21:30:04 -0300

    add attributes to pokemon model

--- pokedex/PokemonDetailViewController.swift
+++ pokedex/PokemonDetailViewController.swift
@@ -12,7 +12,17 @@ class PokemonDetailViewController: UIViewController {
 
     var pokemon: Pokemon!
   
-  @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var pokedexIdLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var nextEvolution: UIImageView!
+  
+  
     override func viewDidLoad() {
         super.viewDidLoad()
       
@@ -23,5 +33,9 @@ class PokemonDetailViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+  
+  
+    @IBAction func backButtonTapped(_ sender: AnyObject) {
+      dismiss(animated: true, completion: nil)
+    }
 }
